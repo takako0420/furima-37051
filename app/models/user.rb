5f941e6aeaf90db9fe769e9e3364c09483742a4b
takako0420 <marl0420boro@gmail.com>
2021-11-03 22:54:27 +0900

    userモデルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,7 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  has_many :items
 end
+
