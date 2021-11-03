f228a11b6c250a2127ccd682d2f39a11ec9748ba
takako0420 <marl0420boro@gmail.com>
2021-11-03 23:50:04 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,14 +5,16 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :user_id
     validates :item_name
-    validates :price
+    validates :price, numericality: {
+      only_integer: true, greater_than_or_equal_to: 300,
+       less_than_or_equal_to: 9999999}
     validates :text
     validates :image
   end
 
 # ActiveHash バリデーション
   with_options numericality: {
-     other_than: 1, message: "can't be blank"} do
+    other_than: 1, message: "can't be blank"} do
     validates :category_id
     validates :condition_id
     validates :prefecture_id
