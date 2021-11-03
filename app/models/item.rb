cc477da117e7de1b5e913076421ace128364dad9
takako0420 <marl0420boro@gmail.com>
2021-11-03 21:05:41 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,32 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+# item バリデーション
+  with_options presence: true do
+    validates :user_id
+    validates :item_name
+    validates :price
+    validates :text
+    validates :image
+  end
+
+# ActiveHash バリデーション
+  with_options numericality: {
+     other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :shipping_fee_id
+  end
+
+# アソシエーション
+  belongs_to :user
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :shipping_fee
 end
+
