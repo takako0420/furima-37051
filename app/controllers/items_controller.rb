d31f7f739c5a293c119866001aea4b859c9398cc
takako0420 <marl0420boro@gmail.com>
2021-11-03 22:54:51 +0900

    パラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to "/"
+    else
+      render new_item_path
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :image,:item_name,:price,:text,:category_id,:condition_id,
+      :shipping_fee_id,:prefecture_id,:shipping_date_id
+    ).merge(user_id: current_user.id)
   end
 end
