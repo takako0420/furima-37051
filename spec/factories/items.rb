ccc6c7b295cf44f838e6e459f3cd918832589c0d
takako0420 <marl0420boro@gmail.com>
2021-11-04 17:23:17 +0900

    単体テストの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
+    item_name          { Faker::Lorem.sentence }
+    price              { rand(300..9_999_999) }
+    text               { Faker::Lorem.sentence }
     
+    category_id        { rand(2..11) }
+                  
+    condition_id       { rand(2..7) }
+    shipping_fee_id    { rand(2..3) }
+    prefecture_id      { rand(2..48) }
+    shipping_date_id   { rand(2..4) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
