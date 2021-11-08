e0d37101ae1805a41c8351333a8c7f6222aac903
takako0420 <marl0420boro@gmail.com>
2021-11-08 13:24:39 +0900

    index、createアクション作成

--- app/controllers/tradings_controller.rb
+++ app/controllers/tradings_controller.rb
@@ -1,12 +1,29 @@
 class TradingsController < ApplicationController
+  before_action :set_item, only:[:index, :create]
 
   def index
-  end
-
-  def new
+    @trading_buyer = TradingBuyer.new
   end
   
   def create
+    @trading_buyer = TradingBuyer.new(trading_params)
+    if @trading_buyer.valid?
+      @trading_buyer.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def trading_params
+    params.require(:trading_buyer).permit(
+      :post_number, :prefecture_id, :city, :address, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
