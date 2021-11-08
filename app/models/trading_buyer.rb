270ccf240ba406366bc4e2c92bb414a7f082dda8
takako0420 <marl0420boro@gmail.com>
2021-11-09 02:30:17 +0900

    tokenのバリデーション追加

--- app/models/trading_buyer.rb
+++ app/models/trading_buyer.rb
@@ -1,10 +1,11 @@
 class TradingBuyer
   include ActiveModel::Model
-    attr_accessor :item_id, :user_id, :post_number, :prefecture_id, :city, :address, :building_name, :phone_number, :token
+    attr_accessor :item_id, :user_id, :post_number, :prefecture_id, :city, :address, :building_name, :phone_number, :trading_id, :token
 
   with_options presence: true do
     validates :item_id
     validates :user_id
+    validates :token
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
@@ -18,5 +19,4 @@ class TradingBuyer
     trading = Trading.create(item_id: item_id, user_id: user_id)
     Buyer.create(post_number: post_number, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, trading_id: trading.id )
   end
-
 end
