810a13bb5bacf04748ba707e505bbb8b0e966da0
takako0420 <marl0420boro@gmail.com>
2021-11-08 13:23:25 +0900

    itemモデル表示の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,23 +6,14 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name
     validates :text
-    validates :price, numericality: {
-      with: /\A[0-9]+\z/,
-      message: 'is invalid. Input half-width characters'
-    }
+    validates :price, numericality: {with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters'}
   end
 
-  validates :price, numericality: {
-    greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-    message: 'Out of setting range'
-  }
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range'}
 
   # ActiveHash バリデーション
   with_options presence: true do
-    with_options numericality: {
-      other_than: 1,
-      message: "can't be blank"
-    } do
+    with_options numericality: {other_than: 1,message: "can't be blank"} do
       validates :category_id
       validates :condition_id
       validates :prefecture_id
